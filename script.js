790e64953dca4f389e2174c90afeaabfe7130528
Sv18-06 <110330011+Sv18-06@users.noreply.github.com>
2022-08-02 10:39:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let max=document.querySelector('input[name="20"]');
+let min=document.querySelector('input[name="10"]');
+
+let random=Math.floor(min+Math.random() * (max-min+1));
+console.log(rand);
+
+
+
+console.log(Math.random())
